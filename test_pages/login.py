f7f01e2d2d67ac6b66ecc454b8368ed735f531dc
Daniela-git <dhiguitaa17@gmail.com>
2024-12-17 13:20:35 -0500

    close pop up

--- test_pages/login.py
+++ test_pages/login.py
@@ -4,11 +4,15 @@ class LoginPage():
     username = "#signin_username"
     continueBtn = "#submit_login"
     password = "#signin_password"
+    closePopUp= ".cerrar"
 
     def __init__(self,page:Page):
       self.page = page
     
     def login(self,user, password):
+      popUp = self.page.locator(self.closePopUp)
+      if(popUp.is_visible()):
+        popUp.click()
       self.page.locator(self.username).fill(user)
       self.page.locator(self.continueBtn).first.click()
       self.page.locator(self.password).fill(password)
