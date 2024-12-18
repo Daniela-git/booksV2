9a0c8d325b960b8d0303031d6de1b894f48b4200
Daniela-git <dhiguitaa17@gmail.com>
2024-12-18 08:22:15 -0500

    to use all books

--- test_pages/myList.py
+++ test_pages/myList.py
@@ -16,7 +16,7 @@ class MyListPage:
         self.page.locator(self.myList).click(force=True)
         self.page.wait_for_load_state("networkidle")  
         books = self.page.locator(self.bookList).all()
-        for book in books[:4]:
+        for book in books:
             title = book.locator(self.title).inner_text()
             regularPrice = book.locator(self.regularPrice).inner_text()
             currentPrice = book.locator(self.currentPrice).inner_text()
